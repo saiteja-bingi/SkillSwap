643a665f5cc6f381bf691e1d2a31a7b5319d7996
Saiteja Bingi <saiteja1973b@gmail.com>
2026-05-25 11:33:15 +0530

    fixed production realtime messaging

--- server/src/socket/socket.js
+++ server/src/socket/socket.js
@@ -8,7 +8,6 @@ export const initializeSocket = (server) => {
     const io = new Server(server, {
         cors: {
             origin: [
-                "http://localhost:5173",
                 "https://skill-swap-ten-vert.vercel.app"
             ],
             methods: ["GET", "POST"]
