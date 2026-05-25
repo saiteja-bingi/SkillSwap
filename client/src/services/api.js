c397fd8313d86ae4256117a5cb3dba5c8a44c135
Saiteja Bingi <saiteja1973b@gmail.com>
2026-05-25 12:05:49 +0530

    fixed production realtime messaging

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,18 +1,25 @@
 import axios from "axios";
 
+const API_URL =
+    import.meta.env.MODE === "development"
+        ? "http://localhost:5000/api"
+        : "https://skillswap-zrev.onrender.com/api";
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+    baseURL: API_URL,
+    withCredentials: true
 });
 
-api.interceptors.request.use(
-  (config) => {
+api.interceptors.request.use((config) => {
+
     const token = localStorage.getItem("token");
+
     if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
+        config.headers.Authorization =
+            `Bearer ${token}`;
     }
+
     return config;
-  },
-  (error) => Promise.reject(error)
-);
+});
 
 export default api;
